ce4a69ab599632f0cc9def65ebfbc649e25148e2
wyann <wyann0202@gmail.com>
2024-06-27 16:19:03 +0800

    fix layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Link from "next/link";
 export default function Home() {
   return (
-    <div className="relative flex min-h-screen flex-col items-center justify-between p-4">
+    <div className="relative flex min-h-screen  h- flex-col">
       {/* <Image
         src="/logo-no-background.svg"
         alt="Hero Image"
@@ -16,19 +16,16 @@ export default function Home() {
         className="hero min-h-screen bg-contain"
         style={{
           backgroundImage: "url('/logo-no-background.svg')",
-          backgroundPosition: "center -10em",
+          backgroundPosition: "-10px",
         }}
       >
-        <div className=""></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <Link
-              href="/market"
-              className="btn btn-outline text-white btn-small btn-wide rounded-md btn-md text-lg btn-ghost hover:bg-white hover:text-black bg-zinc-900"
-            >
-              GET STARTED
-            </Link>
-          </div>
+        <div className="hero-content max-w-md mb-80">
+          <Link
+            href="/market"
+            className="btn btn-outline text-white btn-small btn-wide rounded-md btn-md text-lg btn-ghost hover:bg-white hover:text-black bg-zinc-900"
+          >
+            GET STARTED
+          </Link>
         </div>
       </div>
     </div>
